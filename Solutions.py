47669e4c161c39e21d65115868a323426b95a48a
NarutoHooda <104371211+NarutoHooda@users.noreply.github.com>
2022-04-25 19:47:14 +0530

    Add files via upload

---
+++ Solutions.py
@@ -0,0 +1,30 @@
+Python 3.9.11 (tags/v3.9.11:2de452f, Mar 16 2022, 14:33:45) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/Administrator/Desktop/Himanshu Hooda Assignment1.py ====
+        Question-1
+Enter first number:
+ 2
+Enter second number:
+ 11
+Enter third number:
+ 21
+The average of three numbers is:  11.333333333333334
+        Question-2
+Enter your Gross income:
+$ 50000
+Enter number of dependents:
+ 2
+income Tax is:
+$  6800.0
+        Question-3
+[SID, Name, Gender, Course Name, CGPA]
+[21102015, 'Himanshu Hooda', 'M', 'Civil Engineering', 9.8]
+[21112115, 'Himanshu', 'M', 'Civil Engineering', 8.8]
+        Question-4
+[69, 73, 75, 77, 78]
+        Question-5(a)
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+        Question-5(b)
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+>>> 
